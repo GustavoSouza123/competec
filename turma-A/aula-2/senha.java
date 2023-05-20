06de25735f1ce0156940247b6880186c813e4f1e
GustavoSouza123 <89879590+GustavoSouza123@users.noreply.github.com>
2023-05-20 13:40:46 +0000

    desafio senha

--- turma-A/aula-2/senha.java
+++ turma-A/aula-2/senha.java
@@ -1,3 +1,30 @@
+import java.util.Scanner;
+
 public class senha {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // criando uma senha
+        String senha;
+        System.out.print("Crie uma senha: ");
+        senha = sc.nextLine();
+
+        // verificação
+        String input;
+        boolean acessoPermitido = false;
+        System.out.println();
+
+        while(!acessoPermitido) {
+            System.out.print("Entre com a senha: ");
+            input = sc.nextLine();
+            if(input.equals(senha)) {
+                System.out.println("Acesso permitido!");
+                acessoPermitido = true;
+            } else {
+                System.out.println("Senha inválida\n");
+            }
+        }
+
+        sc.close();
+    }
 }
