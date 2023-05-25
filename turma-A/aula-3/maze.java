7288fa2290ac07c227121b949e9c190df369e279
GustavoSouza123 <89879590+GustavoSouza123@users.noreply.github.com>
2023-05-25 12:35:01 +0000

    criação dos arraylists/hashmap

---
+++ turma-A/aula-3/maze.java
@@ -0,0 +1,26 @@
+import java.util.Scanner; // import the Scanner class
+import java.util.ArrayList; // import the ArrayList class
+
+public class maze {
+    public static void main(String[] args) {
+        // scanner and variables
+        // Scanner sc = new Scanner(System.in);
+        int i;
+
+        // ArrayList for coordinates
+        ArrayList<ArrayList<Integer>> coords = new ArrayList<ArrayList<Integer>>();
+        ArrayList<Integer> x = new ArrayList<Integer>();
+
+        //  board size
+        int boardX = 5;
+        int boardY = 5;
+
+        for(i = 0; i < boardX; i++) {
+            coords.add()
+        }
+
+
+        // print the HashMap
+        System.out.println(coord);
+    }
+}
