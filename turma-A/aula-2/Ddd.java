d7a9e943409da024c9a680793ad10f2358deb8b8
GustavoSouza123 <89879590+GustavoSouza123@users.noreply.github.com>
2023-06-08 20:56:02 +0000

    capitalized classes

--- turma-A/aula-2/Ddd.java
+++ turma-A/aula-2/Ddd.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class ddd {
+public class Ddd {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
--- turma-A/aula-2/Tabuada.java
+++ turma-A/aula-2/Tabuada.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class tabuada {
+public class Tabuada {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
