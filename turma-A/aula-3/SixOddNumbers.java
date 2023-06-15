f24cef02a43dc6ce1846096a6c56d95a1cbe45a8
GustavoSouza123 <89879590+GustavoSouza123@users.noreply.github.com>
2023-06-15 12:29:02 +0000

    close scanner: six odd numbers

--- turma-A/aula-3/SixOddNumbers.java
+++ turma-A/aula-3/SixOddNumbers.java
@@ -4,6 +4,7 @@ public class SixOddNumbers {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int X = sc.nextInt();
+        sc.close();
         if(X % 2 == 0) X += 1;
         for(int i = X; i < X+12; i+=2) {
             if(i % 2 == 1) System.out.println(i);
