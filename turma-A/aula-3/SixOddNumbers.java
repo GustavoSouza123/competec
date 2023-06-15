e1c3fbdc2a6f082f02ca0e1f2e958739103248be
GustavoSouza123 <89879590+GustavoSouza123@users.noreply.github.com>
2023-06-15 12:28:21 +0000

    new finalized: six odd numbers

---
+++ turma-A/aula-3/SixOddNumbers.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class SixOddNumbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int X = sc.nextInt();
+        if(X % 2 == 0) X += 1;
+        for(int i = X; i < X+12; i+=2) {
+            if(i % 2 == 1) System.out.println(i);
+        }
+    }
+}
