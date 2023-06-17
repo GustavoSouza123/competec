dcb045954c8da619c976066986c461bc77a04b25
GustavoSouza123 <89879590+GustavoSouza123@users.noreply.github.com>
2023-06-17 14:30:59 +0000

    finalized: banknotes and coins

--- turma-A/aula-3/BanknotesAndCoins.java
+++ turma-A/aula-3/BanknotesAndCoins.java
@@ -5,30 +5,51 @@ public class BanknotesAndCoins {
         Scanner sc = new Scanner(System.in);
 
         double N = sc.nextDouble();
-        System.out.println(N);
-
-        double C = N % 1;
-        N -= C;
-
-        while(N > 0) {
-            System.out.println(N/100 + " nota(s) de R$ 100,00");
-            N = N % 100;
-            System.out.println(N/50 + " nota(s) de R$ 50,00");
-            N = N % 50;
-            System.out.println(N/20 + " nota(s) de R$ 20,00");
-            N = N % 20;
-            System.out.println(N/10 + " nota(s) de R$ 10,00");
-            N = N % 10;
-            System.out.println(N/5 + " nota(s) de R$ 5,00");
-            N = N % 5;
-            System.out.println(N/2 + " nota(s) de R$ 2,00");
-            N = N % 2;
-            System.out.println(N/1 + " nota(s) de R$ 1,00");
-            N = N % 1;
-        }
-
-        System.out.println("coins: "+C);
-
+        double C = 0;
+        int aux;
         sc.close();
+
+        System.out.println("NOTAS:");
+        aux = (int) N/100;
+        System.out.println(aux + " nota(s) de R$ 100.00");
+        N = N % 100;
+        aux = (int) N/50;
+        System.out.println(aux + " nota(s) de R$ 50.00");
+        N = N % 50;
+        aux = (int) N/20;
+        System.out.println(aux + " nota(s) de R$ 20.00");
+        N = N % 20;
+        aux = (int) N/10;
+        System.out.println(aux + " nota(s) de R$ 10.00");
+        N = N % 10;
+        aux = (int) N/5;
+        System.out.println(aux + " nota(s) de R$ 5.00");
+        N = N % 5;
+        aux = (int) N/2;
+        System.out.println(aux + " nota(s) de R$ 2.00");
+        N = N % 2;
+        C = N * 100;
+        C = Math.round(C);
+        N = 0;
+
+        System.out.println("MOEDAS:");
+        aux = (int) C/100;
+        System.out.println(aux + " moeda(s) de R$ 1.00");
+        C = C % 100;
+        aux = (int) C/50;
+        System.out.println(aux + " moeda(s) de R$ 0.50");
+        C = C % 50;
+        aux = (int) C/25;
+        System.out.println(aux + " moeda(s) de R$ 0.25");
+        C = C % 25;
+        aux = (int) C/10;
+        System.out.println(aux + " moeda(s) de R$ 0.10");
+        C = C % 10;
+        aux = (int) C/5;
+        System.out.println(aux + " moeda(s) de R$ 0.05");
+        C = C % 5;
+        aux = (int) C/1;
+        System.out.println(aux + " moeda(s) de R$ 0.01");
+        C = C % 1;
     }
 }
