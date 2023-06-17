afd8edd4305af4840c44c10542134a998726c459
GustavoSouza123 <89879590+GustavoSouza123@users.noreply.github.com>
2023-06-17 14:36:40 +0000

    new: animal

---
+++ turma-A/aula-4/Animal.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Animal {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input1 = sc.nextLine();
+        String input2 = sc.nextLine();
+        String input3 = sc.nextLine();
+
+        if(input1.equals("vertebrado")) {
+            
+        } else if(input1.equals("invertebrado")) {
+
+        }
+    }
+}
