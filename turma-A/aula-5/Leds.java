16081558b87d75e50350fa4186f90477d0569d2a
GustavoSouza123 <gustavoelia7@gmail.com>
2023-07-01 14:26:50 +0000

    exercise: leds (java)

--- turma-A/aula-5/Leds.java
+++ turma-A/aula-5/Leds.java
@@ -1,15 +1,25 @@
 import java.util.Scanner;
 
 public class Leds {
+    // this code is not working
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int i, j;
+
         int N = sc.nextInt();
         String V;
-        char[] arr;
+        int[] arrLeds = {6,2,5,5,4,5,6,3,7,6};
+        int total = 0;
         while(N > 0) {
             V = sc.nextLine();
-            arr = V.toCharArray();
-            for(int i = 0; i < arr.length; i++) System.out.println(arr[i] + ", ");
+            for(i = 0; i < V.length(); i++) {
+                for(j = 0; j < arrLeds.length; j++) {
+                    if(V.charAt(i) == j) total += arrLeds[j];
+                }
+            }
+            System.out.println(total + " leds");
+            total = 0;
+            N--;
         }
     }
 }
