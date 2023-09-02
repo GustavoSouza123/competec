d2e052eeea252aa682284d4d26826b2e0ce6aea4
Gustavo Souza <gustavoelia7@gmail.com>
2023-09-02 10:18:42 -0300

    Slugs.java: identation

--- turma-A/aula-9/Slugs.java
+++ turma-A/aula-9/Slugs.java
@@ -3,19 +3,19 @@ import java.util.Scanner;
 public class Slugs {
     
 	public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int L, nivel, velocidade;
-        while (sc.hasNext()) {
-        	L = sc.nextInt();
-        	nivel = 0;
-        	for(int i = 0; i < L; i++) {
-        		velocidade = sc.nextInt();
-        		if(velocidade < 10 && nivel <= 1)  nivel = 1;
-        		else if(velocidade >= 20 && nivel <= 3) nivel = 3;
-        		else if(nivel < 2) nivel = 2;
-        	}
-        	System.out.println(nivel);
-        }
+	        Scanner sc = new Scanner(System.in);
+	        int L, nivel, velocidade;
+	        while (sc.hasNext()) {
+	        	L = sc.nextInt();
+	        	nivel = 0;
+	        	for(int i = 0; i < L; i++) {
+	        		velocidade = sc.nextInt();
+	        		if(velocidade < 10 && nivel <= 1)  nivel = 1;
+	        		else if(velocidade >= 20 && nivel <= 3) nivel = 3;
+	        		else if(nivel < 2) nivel = 2;
+	        	}
+	        	System.out.println(nivel);
+	        }
 	}
 	
 }
